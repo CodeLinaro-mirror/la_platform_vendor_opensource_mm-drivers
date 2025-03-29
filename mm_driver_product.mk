a42c8c323b1bc8dd4f92f75226f6d03d9c7da301
Sai Srujana Oruganti <quic_osaisruj@quicinc.com>
2025-03-29 14:07:43 +0530

    hfi_core: enable hfi_core compilation for aspen
    Enable compilation hfi core driver for aspen
    target.
    
    Change-Id: I4f304735d74a51750a5f772a2c233ef0a06e03c9
    Signed-off-by: Sai Srujana Oruganti <quic_osaisruj@quicinc.com>

--- mm_driver_product.mk
+++ mm_driver_product.mk
@@ -8,11 +8,17 @@ endif
 
 ifeq ($(MM_DRV_DLKM_ENABLE), true)
 	ifneq ($(TARGET_BOARD_PLATFORM), taro)
-		PRODUCT_PACKAGES += sync_fence.ko msm_ext_display.ko msm_hw_fence.ko
-		DISPLAY_MM_DRIVER += sync_fence.ko msm_ext_display.ko msm_hw_fence.ko
-		ifeq ($(filter $(TARGET_BOARD_PLATFORM), canoe),$(TARGET_BOARD_PLATFORM))
+		PRODUCT_PACKAGES += sync_fence.ko msm_ext_display.ko
+		DISPLAY_MM_DRIVER += sync_fence.ko msm_ext_display.ko
+	endif
+
+	ifneq ($(filter $(TARGET_BOARD_PLATFORM), vienna),$(TARGET_BOARD_PLATFORM))
+		PRODUCT_PACKAGES += msm_hw_fence.ko
+		DISPLAY_MM_DRIVER += msm_hw_fence.ko
+	endif
+
+	ifeq ($(filter $(TARGET_BOARD_PLATFORM), canoe vienna),$(TARGET_BOARD_PLATFORM))
 			PRODUCT_PACKAGES += msm_hfi_core.ko
 			DISPLAY_MM_DRIVER += msm_hfi_core.ko
-		endif
 	endif
 endif
