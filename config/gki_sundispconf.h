81e7e4a6f922864682f4cd84a618d879b87d27af
Haihua Nie <quic_nie@quicinc.com>
2025-04-17 14:43:32 +0800

    mm-drivers: add makefile and config for mm-drivers build
    The compilation of display-driver depends on mm-drivers, add makefile
    and config to support mm-drivers compilation
    
    Change-Id: I03f102560773afa16260c34d9fae33b5707380c1
    Signed-off-by: Haihua Nie <quic_nie@quicinc.com>

---
+++ config/gki_sundispconf.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#define CONFIG_MSM_EXT_DISPLAY 1
+#define CONFIG_QCOM_SPEC_SYNC 1
+#define CONFIG_QTI_HW_FENCE 1
