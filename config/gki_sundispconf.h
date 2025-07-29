4ee035e77574bb40d42f7c0cb94101df97f5b565
lingwenk <lingwenk@qti.qualcomm.com>
2025-07-29 20:08:43 +0800

    temp: support mm-drivers
    Change-Id: Ibc0c9e4fe28fa05f06793589a3d82332eebfdd2b
    Signed-off-by: lingwenk <lingwenk@qti.qualcomm.com>
    (cherry picked from commit 70abc3197832248d1ddd7c1c06ac66af5b3a69af)

---
+++ config/gki_sundispconf.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ */
+
+#define CONFIG_MSM_EXT_DISPLAY 1
+#define CONFIG_QCOM_SPEC_SYNC 1
+#define CONFIG_QTI_HW_FENCE 1
