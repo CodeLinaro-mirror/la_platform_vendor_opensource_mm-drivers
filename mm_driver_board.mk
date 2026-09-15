490326c7f766ea7a52f9729d3b3493e9d513cd9d
Sumadhura Kalyan Singamchetti <ssingamc@qti.qualcomm.com>
2026-09-15 13:40:28 +0530

    mm-driver: Update config file for waipio target
    Change-Id: Ib4a3c5e38c91730e6a5992cd06aa363ee8fb2c44
    Signed-off-by: Sumadhura Kalyan Singamchetti <ssingamc@qti.qualcomm.com>

--- mm_driver_board.mk
+++ mm_driver_board.mk
@@ -28,13 +28,13 @@ ifeq ($(MM_DRV_DLKM_ENABLE), true)
 			BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(KERNEL_MODULES_OUT)/sync_fence.ko
 		endif
 
-		ifneq ($(filter $(TARGET_BOARD_PLATFORM), vienna bengal malabar),$(TARGET_BOARD_PLATFORM))
+		ifneq ($(filter $(TARGET_BOARD_PLATFORM), taro vienna bengal malabar),$(TARGET_BOARD_PLATFORM))
 			BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/msm_hw_fence.ko
 			BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/msm_hw_fence.ko
 			BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(KERNEL_MODULES_OUT)/msm_hw_fence.ko
 		endif
 
-		ifeq ($(filter $(TARGET_BOARD_PLATFORM), taro canoe chora vienna seraph),$(TARGET_BOARD_PLATFORM))
+		ifeq ($(filter $(TARGET_BOARD_PLATFORM), canoe chora vienna seraph),$(TARGET_BOARD_PLATFORM))
 			BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/msm_hfi_core.ko
 			BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/msm_hfi_core.ko
 			BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(KERNEL_MODULES_OUT)/msm_hfi_core.ko
